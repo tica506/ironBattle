c1785ff1b151b6fd5e5eb9d21202918e03f6cfdf
Alexia <ahamil20@fau.edu>
2024-06-17 15:19:48 -0400

    update4

--- src/Character.java
+++ src/Character.java
@@ -1,9 +1,40 @@
 public abstract class Character {
-String id;
-String name;
-int hp;
-boolean isAlive = true;
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;private String id;
+private String name;
+private int hp;
+private boolean isAlive = true;
 
 
+    public Character() {
 
+    }
 }
