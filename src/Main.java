7db32d6cf4671ab2e51ba326537693daf6e70cc2
tica506 <93236626+tica506@users.noreply.github.com>
2024-06-19 11:20:35 -0400

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("bye world this is a test!");
-        System.out.println("This is Sofia 06/17/2024");
-        int a = 17;
-        System.out.println("I am " + a + " years old");
-        System.out.println("I am " + a + " Estefany");
-    }
-}
